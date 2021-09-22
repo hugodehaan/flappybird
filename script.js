6d7c9e3cd917304d909ae88364adcb6c550bda2f
Thijs Sytema <57255526+Thijs-Sytema@users.noreply.github.com>
2021-09-22 10:00:58 +0000

    begin aan physics

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
-let
-function setup(){
-  createCanvas(700,600);
-  ball = new Ball();
-}
-class Ball{
-  
-  constructor(x=50, y= height/2, g=0,6, v=0, l=-20 ) {
-    this.x = x;
-    this.y = y;
-    this.gravity = g;
-    this.velocity = v;
-    this.lift = l;
+class Bird {
+  constructor(height) {
+    this.y = 200;
+    this.x = 64;
 
+    this.gravity = 1;
+    this.velocity = 0;
   }
+
+  show() {
+    fill(255);
+    ellipse(this.x, this.y, 32, 32);
+  }
+}
+
+var bird;
+
+function setup() {
+  createCanvas(400, 600);
+  bird = new Bird(30);
+}
+
+function draw() {
+  background(100);
+  bird.show();
 }
