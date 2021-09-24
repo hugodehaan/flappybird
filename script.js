ceb235e05baf5247796ff1d3dfdbc0ef5575e00d
Hugo <45490945+hugodehaan@users.noreply.github.com>
2021-09-24 09:55:45 +0000

    balletje vliegt omhoog

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ class Bird {
     this.y = 200;
     this.x = 64;
 
-    this.gravity = 1;
+    this.gravity = 0.6;
+    this.lift = -15;
     this.velocity = 0;
   }
 
@@ -13,12 +14,23 @@ class Bird {
     ellipse(this.x, this.y, 32, 32);
   }
 
+  up = function() {
+    this.velocity += this.lift;
+    Println(this.velocity);
+  }
+
   update = function(){
     this.velocity += this.gravity;
+    
     this.y += this.velocity;
 
     if (this.y > height) {
       this.y = height;
+      this.velocity = 0;
+    }
+
+    if (this.y < 0) {
+      this.y = 0;
       this.velocity = 0;
     }
   }
@@ -35,4 +47,11 @@ function draw() {
   background(100);
   bird.update();
   bird.show();
+}
+
+function keyPressed() {
+  if (key == ' ') {
+    bird.up();
+
+  }
 }
